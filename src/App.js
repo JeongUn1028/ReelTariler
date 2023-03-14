f57327ffc89a5d229df1cae7faebccaad100b7d6
Teddy <wjddns363@naver.com>
2023-03-14 21:21:30 +0900

    중간커밋

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/part/Header";
+import Movies from "./components/part/Movies";
+import Footer from "./components/part/Footer";
+import Select from "./components/part/Select";
+import styled from "styled-components";
 
-function App() {
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  margin: 0px;
+  padding: 0px;
+`;
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Header />
+      <Select />
+      <Movies />
+      <Footer />
+    </Wrapper>
   );
-}
+};
 
 export default App;
